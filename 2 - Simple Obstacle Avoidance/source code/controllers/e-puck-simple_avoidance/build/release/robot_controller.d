6d32185ab67c90ffcb81de02446d6cb5b897a6fa
Albert Alfrianta <albert.brucelee@gmail.com>
2021-08-07 21:52:59 +0700

    Apply .gitignore

--- 2 - Simple Obstacle Avoidance/source code/controllers/e-puck-simple_avoidance/build/release/robot_controller.d
+++
@@ -1,6 +0,0 @@
-build/release/robot_controller.o: robot_controller.c robot_controller.h \
- /usr/local/webots/include/controller/c/webots/robot.h \
- /usr/local/webots/include/controller/c/webots/types.h \
- /usr/local/webots/include/controller/c/webots/nodes.h \
- /usr/local/webots/include/controller/c/webots/motor.h \
- /usr/local/webots/include/controller/c/webots/distance_sensor.h
